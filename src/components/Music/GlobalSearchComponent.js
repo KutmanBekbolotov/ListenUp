eb659f78ce9f87fe33496a947c057c03816b9a16
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-07-18 20:47:48 +0600

    Add good global search

--- src/components/Music/GlobalSearchComponent.js
+++ src/components/Music/GlobalSearchComponent.js
@@ -2,58 +2,54 @@ import React, { useEffect, Suspense, lazy } from 'react';
 import { useQuery } from 'react-query';
 import Sidebar from '../sidebar';
 import GenreCover from './GenreCover';
+import MediaPlayer from '../MediaPlayer';
 import './GlobalSearch.css';
-import GlobalSearch from './GlobalSearch';
-
 
 const SongSearch = lazy(() => import('../MusicSearcher'));
-const MediaPlayer = lazy(() => import('../MediaPlayer'));
 
 const GlobalSearchComponent = () => {
-    const genresQuery = useQuery('genres', () => [
-        { name: 'Rock', image: 'genre-covers/rock.jpg' },
-        { name: 'Pop', image: 'genre-covers/pop.jpg' },
-    ]);
+  const genresQuery = useQuery('genres', () => [
+    { name: 'Rock', image: 'genre-covers/rock.jpg' },
+    { name: 'Pop', image: 'genre-covers/pop.jpg' },
+  ]);
 
-    useEffect(() => {
-        if (genresQuery.data) {
-            genresQuery.data.forEach((genre) => {
-                if (!localStorage.getItem(genre.name)) {
-                    fetch(genre.image)
-                        .then((response) => response.blob())
-                        .then((blob) => {
-                            const objectURL = URL.createObjectURL(blob);
-                            localStorage.setItem(genre.name, objectURL);
-                        })
-                        .catch((error) => console.error('Ошибка при загрузке изображения:', error));
-                }
-            });
+  useEffect(() => {
+    if (genresQuery.data) {
+      genresQuery.data.forEach((genre) => {
+        if (!localStorage.getItem(genre.name)) {
+          fetch(genre.image)
+            .then((response) => response.blob())
+            .then((blob) => {
+              const objectURL = URL.createObjectURL(blob);
+              localStorage.setItem(genre.name, objectURL);
+            })
+            .catch((error) => console.error('Ошибка при загрузке изображения:', error));
         }
-    }, [genresQuery.data]);
+      });
+    }
+  }, [genresQuery.data]);
 
-    return (
-        <div className="global-search-page">
-            <Sidebar />
-            <div className="content">
-                <header>
-                    <GlobalSearch />
-                    <Suspense fallback={<div>Loading SongSearch...</div>}>
-                        <SongSearch />
-                    </Suspense>
-                </header>
-                <main>
-                    <div className="genres">
-                        {genresQuery.data && genresQuery.data.map((genre) => (
-                            <GenreCover key={genre.name} genre={genre} />
-                        ))}
-                    </div>
-                </main>
-            </div>
-            <Suspense fallback={<div>Loading MediaPlayer...</div>}>
-                <MediaPlayer />
-            </Suspense>
-        </div>
-    );
+  return (
+    <div className="global-search-page">
+      <Sidebar />
+      <div className="content">
+        <header>
+          <h1>Global Search Page</h1>
+          <Suspense fallback={<div>Loading SongSearch...</div>}>
+            <SongSearch />
+          </Suspense>
+        </header>
+        <main>
+          <div className="genres">
+            {genresQuery.data && genresQuery.data.map((genre) => (
+              <GenreCover key={genre.name} genre={genre} />
+            ))}
+          </div>
+        </main>
+      </div>
+      <MediaPlayer />
+    </div>
+  );
 };
 
 export default GlobalSearchComponent;
