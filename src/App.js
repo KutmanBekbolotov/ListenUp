e04ee9a9eb57931aab63867b5d3b1548d476ae92
Kutman Bekbolotov <kutmanbekbolotov@MacBook-Air-Kutman.local>
2024-07-20 19:02:24 +0600

    genre styles changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
 import UploadMusic from './components/Music/UploadMusic';
 import Playlist from './components/Music/PlayList';
 import MediaPlayer from './components/MediaPlayer';
-import GlobalSearch from './components/Music/GlobalSearch'; // Обратите внимание на правильный импорт
+import GlobalSearch from './components/Music/GlobalSearch';
 import GenreSongs from './components/Music/GenreSongs';
-import UpdateMetadata from './components/admin/UpdateMetadata'; // Убедитесь, что путь правильный
-import { AuthProvider } from './context/AuthContext';
+import UpdateMetadata from './components/admin/UpdateMetadata';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import { MediaPlayerProvider } from './context/MediaPlayerContext';
+
+const PrivateRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+  return currentUser ? children : <Navigate to="/global-search" />;
+};
 
 function App() {
   return (
@@ -19,14 +24,15 @@ function App() {
         <Router>
           <div className="App">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/uploadmusic" element={<UploadMusic />} /> {/* Исправлено на нижний регистр для согласованности */}
-              <Route path="/playlist" element={<Playlist />} /> {/* Исправлено на нижний регистр для согласованности */}
-              <Route path="/search" element={<GlobalSearch />} />
-              <Route path="/genre/:genreName" element={<GenreSongs />} />
-              <Route path="/update-metadata" element={<UpdateMetadata />} />
+              <Route path="/uploadmusic" element={<PrivateRoute><UploadMusic /></PrivateRoute>} />
+              <Route path="/playlist" element={<PrivateRoute><Playlist /></PrivateRoute>} />
+              <Route path="/global-search" element={<GlobalSearch />} />
+              <Route path="/genre/:genreName" element={<PrivateRoute><GenreSongs /></PrivateRoute>} />
+              <Route path="/update-metadata" element={<PrivateRoute><UpdateMetadata /></PrivateRoute>} />
+              <Route path="*" element={<Navigate to="/global-search" />} />
             </Routes>
             <MediaPlayer />
           </div>
